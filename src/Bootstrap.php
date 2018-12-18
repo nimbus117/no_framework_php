1cd4876192c96f106315070b5c3f5e3f7cd73753
nimbus117 <mail@nimbus117.co.uk>
2018-12-18 22:12:28 +0000

    added router

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -21,15 +21,53 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
+/**
+ * HTTP component - object-oriented interface
+ */
 $request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
 $response = new \Http\HttpResponse;
 
-$content = '<h1>Hello World</h1>';
-$response->setContent($content);
+$dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/hello-world', function () {
+        echo 'Hello World';
+    });
+    $r->addRoute('GET', '/another-route', function () {
+        echo 'This works too';
+    });
+});
+
+
+/**
+ * router - dispatches to different handlers
+ */
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+switch ($routeInfo[0]) {
+case \FastRoute\Dispatcher::NOT_FOUND:
+  $response->setContent('404 - Page not found');
+  $response->setStatusCode(404);
+  break;
+case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+  $response->setContent('405 - Method not allowed');
+  $response->setStatusCode(405);
+  break;
+case \FastRoute\Dispatcher::FOUND:
+  $handler = $routeInfo[1];
+  $vars = $routeInfo[2];
+  call_user_func($handler, $vars);
+  break;
+}
+
+// $content = '<h1>Hello World</h1>';
+// $response->setContent($content);
 // $response->setContent('404 - Page not found');
 // $response->setStatusCode(404);
 
+
+/**
+ * send the response data to the browser
+ */
 foreach ($response->getHeaders() as $header) {
+  // false = don't overwrite existing headers
   header($header, false);
 }
 
